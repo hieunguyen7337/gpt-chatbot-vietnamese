8c1cfc3eb691253563830519c1d532d94f82ab26
hieunguyen7337 <59214551+hieunguyen7337@users.noreply.github.com>
2023-04-04 08:52:51 +0700

    rename the infer code

--- chatbot-gpt-inference.py
+++ chatbot-gpt-inference.py
@@ -1,25 +1,23 @@
-from transformers import GPT2LMHeadModel,  GPT2Tokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import json
 
-class GPT2_Chatbot():
+class GPT_Chatbot():
     def __init__(self):
-        # Initialize the tokenizer, model configuration and model using the GPT2 pre-trained model from Hugging Face Transformers
-        self.tokenizer = GPT2Tokenizer.from_pretrained("./gpt2_xl_model") 
-        self.model = GPT2LMHeadModel.from_pretrained("./gpt2_xl_model")
+        # Initialize the tokenizer, model configuration and model using the GPTJ6B-VI model from Hugging Face Transformers
+        self.tokenizer = AutoTokenizer.from_pretrained("./gptj6B_VI_model")
+        self.model = AutoModelForCausalLM.from_pretrained("./gptj6B_VI_model", device_map="auto", load_in_8bit=True)
         self.generate_config = json.load(open("generation_config.json"))
 
     def tokenize_text(self, text_input):
-        # inputs = self.tokenizer(text, return_tensors="pt")
-        # input_ids = inputs["input_ids"]
-        tokenized_text_input = torch.tensor(self.tokenizer.encode(text_input)).unsqueeze(0)
+        tokenized_text_input = torch.tensor(self.tokenizer.encode(text_input)).unsqueeze(0).to("cuda")
         return tokenized_text_input
 
     def model_generate_token(self, tokenized_text_input):
         generation_output = self.model.generate(tokenized_text_input,
                                                 do_sample=True,   
-                                                top_k=self.generate_config.get("top_k"), 
-                                                top_p=self.generate_config.get("top_p"),
+                                                top_k=25, 
+                                                top_p=0.95,
                                                 num_return_sequences=1, 
                                                 pad_token_id=self.tokenizer.eos_token_id,
                                                 max_length=len(tokenized_text_input[0]) + 1)
@@ -29,9 +27,9 @@ class GPT2_Chatbot():
         print("Chatbot:",end="")
         input_ids = self.tokenize_text(input_text)
         # while(True):
-        for i in range(15):
+        for i in range(35):
             model_output = self.model_generate_token(input_ids)
-            if model_output[0][-1:] == 198:
+            if model_output[0][-1:] == 172:
                 break
             decoded_text = self.tokenizer.decode(model_output[0][-1:])
             print(decoded_text,end="")
@@ -41,33 +39,32 @@ class GPT2_Chatbot():
         return input_text
 
     def start_chat(self):
-        print("""This is a chatbot create using the Huggingface opensource GPT2-XL model, you can type in
-break to stop the chatbot or restart to restart the chatbot conversation""")
+        print("""Đây là một con chatbot được tạo nhờ model GPTJ, bạn có thể gõ vào 'break' để dừng chatbot hay gõ 'restart' để bắt đầu lại cuộc nói chuyện""")
         # Start the chatbot
         start = True
         while(True):
             if start:
                 # initialize the chatbot with a starting prompt
-                starting_prompt = """You are a chatbot, response to the user question as below."""
+                starting_prompt = """Bạn là một con chatbot tên là GPTJ, hãy trả lời câu hỏi theo như mẫu dưới đây"""
                 context = starting_prompt 
                 start = False
             else:
                 context = output_text
             
-            inp = input("User: ")
+            inp = input("Người dùng: ")
             # Allow the user to break out of the conversation or restart it
             if inp == "break":
-                print("Goodbye")
+                print("tạm biệt")
                 break
             if inp == "restart":
                 print("----------------------------")
                 start = True
                 continue
 
-            input_text = context + f"\nUser: {inp}\nChatbot:"
+            input_text = context + f"\nNgười dùng: {inp}\nChatbot:"
             output_text = self.model_generate(input_text)
 
 if __name__ == '__main__':
     # Create an instance of the Chatbot class and start the chat
-    chatbot = GPT2_Chatbot()
+    chatbot = GPT_Chatbot()
     chatbot.start_chat()
